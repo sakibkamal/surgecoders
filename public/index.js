6feaf386329a753a8f6ca7051cc432e6d4262907
Sakib Bin Kamal <75124814+sakibkamal@users.noreply.github.com>
2021-04-24 01:34:48 +0600

    Delete public directory

--- public/index.js
+++
@@ -1,8 +0,0 @@
-//grab everything we need
-const btn = document.querySelector('button.mobile-menu-button')
-const menu = document.querySelector('.mobile-menu')
-
-//add event listener
-btn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-});
